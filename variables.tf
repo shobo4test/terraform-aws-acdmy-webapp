4e147a8c9849cf97b599ca0ab1f5896f5c77dc0e
lndmijxieatf <lndmijxieatf@example.com>
2023-02-02 13:14:15 +0000

    Adding the webapp module

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "AWS Cloud Hosting Region"
+  default = "us-east-2"
+}
+
+variable "prefix" {
+  description = "Prefix for the webapp name"
+  default     = "academy-webapp"
+}
+
+variable "name" {
+  description = "Name for webapp"
+  default     = "lndmijxieatf"
+}
